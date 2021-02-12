650d694f535de31eb8fa8c9016c761899baaa940
Isaac Steele <isaacsteele@Isaacs-MacBook-Pro.local>
2021-02-11 20:07:19 -0500

    Added a birthday method to ask the user for age and date of birth and then returning the info to the user.

--- src/Greeter.java
+++ src/Greeter.java
@@ -2,12 +2,35 @@ import java.util.Scanner;
 
 public class Greeter {
 	
-	public static void main(String[] args) {
-		
-	Scanner scan = new Scanner(System.in);
+	static Scanner scan = new Scanner(System.in);
+	
+	public static void Greeter() {
+
 	System.out.println("Hello, what is your name?");
 	String name = scan.nextLine();
 	
 	System.out.println("What's up " + name + " it's nice to meet you!");
+	}
+	
+	public static void Birthday() {
+		System.out.println("Please enter your age and date of birth 00/00/0000");
+		System.out.println("Age: ");
+		int age = scan.nextInt();
+		System.out.println("Month: ");
+		int month = scan.nextInt();
+		System.out.println("Day:");
+		int day = scan.nextInt();
+		System.out.println("Year:");
+		int year = scan.nextInt();
+		
+		System.out.println("Age: " + age);
+		System.out.println("DOB: " + month + "/" + day + "/" + year);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Greeter();
+		Birthday();
 	}
 }
